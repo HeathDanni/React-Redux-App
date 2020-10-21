486f7474df3547f2d2c11a45a41a4f703e64dc2a
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-20 19:10:00 -0600

    connected component

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import goatPicture from './components/goatPicture';
 
 function App() {
   return (
     <div className="App">
       <h1>Look at these cute goats!</h1>
+      <goatPicture/>
     </div>
   );
 }
--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from redux;
-import {Provider} from react-redux;
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import Reducer from './reducers';
 
-let store = createStore();
+let store = createStore(Reducer);
 
 ReactDOM.render(
   <Provider store={store}>
